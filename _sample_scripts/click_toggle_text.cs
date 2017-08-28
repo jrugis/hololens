43b4440e700776bc67144504c605d3c9df8f91a5
John Rugis <jrugis@sc-math-316974.UoA.auckland.ac.nz>
2017-08-28 19:39:03 +1200

    more scripts

---
+++ _sample_scripts/click_toggle_text.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.WSA.Input;
+
+public class clicked : MonoBehaviour {
+    GestureRecognizer gr = null;
+
+    void Start () {
+        gr = new GestureRecognizer();
+        gr.TappedEvent += TappedEventDelegate;
+        gr.StartCapturingGestures();
+    }
+    void TappedEventDelegate(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        if (source == InteractionSourceKind.Hand)
+        {
+            GetComponent<Text>().enabled = !GetComponent<Text>().enabled;
+        }
+    }
+}
